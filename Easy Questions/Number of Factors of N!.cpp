fd9d89e89875340a1986fc74adfc643415205e6a
Vishal Kumar <vishalsharma.viz@gmail.com>
2022-11-05 08:27:03 +0530

    Create Number of Factors of N!.cpp

---
+++ Easy Questions/Number of Factors of N!.cpp
@@ -0,0 +1,50 @@
+/*Given a positive integer N, find the number of factors in N! ( N factorial).*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long factorsOfFactorial(int N)
+    {
+        long long count = 1;
+        
+        for(int i = 1 ; i <= N ; i++){
+            count = count*i;
+            
+        }
+        
+        long long ans = 0;
+        
+        for(int i = 1 ; i <=count ; i++){
+            if(count % i ==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.factorsOfFactorial(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+ 
